20686075be03e8dd9abb0f0df7d74f70375e4b36
Joschua <70809675+selfire1@users.noreply.github.com>
2025-09-06 15:30:07 +1000

    set up linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,14 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from '@eslint/eslintrc';
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
 });
 
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
+  ...compat.config(({
+    extends: ["next", "prettier"]
+  }))
 ];
 
 export default eslintConfig;
