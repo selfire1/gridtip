7840172f1cf77919a0f45f9f0f9e2fc2668ce184
Joschua <70809675+selfire1@users.noreply.github.com>
2025-09-06 15:40:55 +1000

    set up prettier via eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,24 @@
-import { FlatCompat } from '@eslint/eslintrc';
+import { FlatCompat } from '@eslint/eslintrc'
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
-});
-
+})
 
 const eslintConfig = [
-  ...compat.config(({
-    extends: ["next", "prettier"]
-  }))
-];
+  ...compat.config({
+    extends: ['next', 'prettier'],
+    plugins: ['prettier'],
+    rules: {
+      'prettier/prettier': [
+        'warn',
+        {
+          singleQuote: true,
+          semi: false,
+          jsxSingleQuote: true,
+        },
+      ],
+    },
+  }),
+]
 
-export default eslintConfig;
+export default eslintConfig
