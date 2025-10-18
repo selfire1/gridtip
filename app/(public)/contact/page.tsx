219e3173961bd02a2b332d8d2436ccf0a5db644a
Joschua <70809675+selfire1@users.noreply.github.com>
2025-10-19 07:46:03 +1000

    break contact cards into component

--- app/(public)/contact/page.tsx
+++ app/(public)/contact/page.tsx
@@ -1,3 +1,27 @@
+import ContactCards from '@/components/contact-cards'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Contact',
+  description:
+    'Get in touch with the GridTip team to report an issue or send a suggestion.',
+}
+
 export default function ContactPage() {
-  return <pre>TODO: Contact page</pre>
+  return (
+    <div className='py-12 is-container space-y-12'>
+      <div className='text-center max-w-prose mx-auto space-y-4'>
+        <h1 className='text-primary leading-tighter text-4xl font-semibold tracking-tight text-balance lg:leading-[1.1] xl:text-5xl xl:tracking-tighter'>
+          Contact
+        </h1>
+        <p className='text-balance text-muted-foreground'>
+          Get in touch to report in issue or just to say hi!
+        </p>
+      </div>
+
+      <div className='grid gap-8 sm:grid-cols-2'>
+        <ContactCards />
+      </div>
+    </div>
+  )
 }
