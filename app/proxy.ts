8f4a498fff29a9ff422778a20141e970a3936111
Joschua <70809675+selfire1@users.noreply.github.com>
2025-10-24 16:13:29 +1000

    upgrade to next 16

--- app/proxy.ts
+++ app/proxy.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import { QueryOrigin } from '@/constants'
 import { getCookieCache } from 'better-auth/cookies'
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const session = await getCookieCache(request)
 
   if (!session) {
