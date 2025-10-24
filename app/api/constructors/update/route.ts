0096f93ca531eeffd4a2333aad2899f0bac9977a
Joschua <70809675+selfire1@users.noreply.github.com>
2025-10-24 17:29:16 +1000

    add update constructors route

--- app/api/constructors/update/route.ts
+++ app/api/constructors/update/route.ts
@@ -1,15 +1,69 @@
 import { CacheTag } from '@/constants/cache'
 import { revalidateTag } from 'next/cache'
 import { NextRequest } from 'next/server'
+import { createResponse, fetchJolpica, validateToken } from '../../utils'
+import { ConstructorsResponse } from '@/types/ergast'
+import { db } from '@/db'
+import { constructorsTable } from '@/db/schema/schema'
+import { sql } from 'drizzle-orm'
 
 export const GET = async (_request: NextRequest) => {
+  const validationResponse = await validateToken()
+  if (!validationResponse.ok) {
+    return validationResponse
+  }
+
+  const constructors = await getConstructors()
+  if (!constructors?.length) {
+    return createResponse(404, 'No constructors found')
+  }
+
+  const ids = await setConstructorsInDatabase(constructors)
+
   revalidateTag(CacheTag.Constructors)
 
-  // TODO: implement
+  return createResponse(201, {
+    updated: ids.length,
+    received: constructors.length,
+  })
 
-  return new Response(
-    JSON.stringify({
-      status: 200,
-    }),
-  )
+  async function getConstructors() {
+    const response = await fetchJolpica<ConstructorsResponse>(
+      '/ergast/f1/2025/constructors/',
+    )
+    return response.MRData.ConstructorTable.Constructors
+  }
+
+  async function setConstructorsInDatabase(
+    constructors: ConstructorsResponse['MRData']['ConstructorTable']['Constructors'],
+  ) {
+    const returning = await db
+      .insert(constructorsTable)
+      .values(
+        constructors.map((constructor) => {
+          if (!constructor.constructorId) {
+            throw new Error('No constructorId included')
+          }
+          return {
+            id: constructor.constructorId,
+            name: constructor.name,
+            nationality: constructor.nationality ?? '',
+            lastUpdated: new Date(),
+          }
+        }),
+      )
+      .onConflictDoUpdate({
+        target: constructorsTable.id,
+        set: {
+          name: sql`excluded.name`,
+          nationality: sql`excluded.nationality`,
+          lastUpdated: sql`excluded.last_updated`,
+        },
+      })
+
+      .returning({
+        id: constructorsTable.id,
+      })
+    return returning
+  }
 }
---
+++ app/api/utils.ts
@@ -0,0 +1,62 @@
+import 'server-only'
+
+import { ofetch } from 'ofetch'
+import { headers as getHeaders } from 'next/headers'
+import bcrypt from 'bcryptjs'
+import { NextResponse } from 'next/server'
+
+/**
+ * @link https://github.com/jolpica/jolpica-f1/blob/main/docs/README.md
+ */
+export const fetchJolpica = ofetch.create({
+  baseURL: 'https://api.jolpi.ca',
+})
+
+export async function validateToken() {
+  const headers = await getHeaders()
+
+  const authHeader = headers.get('authorization')
+
+  if (!authHeader || !authHeader.startsWith('Basic ')) {
+    const response = createResponse(401, 'Invalid header')
+    response.headers.set('WWW-Authenticate', 'Basic realm="Protected Area"')
+    return response
+  }
+
+  const credentials = Buffer.from(authHeader.split(' ')[1], 'base64').toString()
+  const [providedUsername, providedPassword] = credentials.split(':')
+
+  const storedHashedPassword = process.env.UPDATES_PASSWORD_HASH as string
+  const storedUser = process.env.UPDATES_USER as string
+
+  const isPasswordValid = await bcrypt.compare(
+    providedPassword,
+    storedHashedPassword,
+  )
+  const isUserValid = providedUsername === storedUser
+  if (!isPasswordValid || !isUserValid) {
+    return createResponse(401, 'Invalid credentials')
+  }
+  return {
+    ok: true,
+  }
+}
+
+export function createResponse(
+  status: number,
+  jsonOrMessage: Record<string, any> | string,
+) {
+  if (!status.toString().startsWith('2')) {
+    return NextResponse.json(
+      {
+        error: jsonOrMessage,
+      },
+      { status },
+    )
+  }
+  const body =
+    typeof jsonOrMessage === 'string'
+      ? { message: jsonOrMessage }
+      : jsonOrMessage
+  return NextResponse.json(body, { status })
+}
