c9fb8a9d310e88722bd4b9d26a77128e3ceb0831
Joschua <70809675+selfire1@users.noreply.github.com>
2025-10-25 10:26:38 +1000

    wrap auth in suspense
    to solve build error

--- app/(standalone)/auth/page.tsx
+++ app/(standalone)/auth/page.tsx
@@ -1,11 +1,16 @@
 import { LoginForm } from '@/components/login-form'
-import { AppHeader } from '@/components/app-header'
 import { Metadata } from 'next'
+import { Skeleton } from '@/components/ui/skeleton'
+import { Suspense } from 'react'
 
 export const metadata: Metadata = {
   title: 'Log in or Sign up',
 }
 
 export default function AuthPage() {
-  return <LoginForm />
+  return (
+    <Suspense fallback={<Skeleton className='h-96 w-prose' />}>
+      <LoginForm />
+    </Suspense>
+  )
 }
