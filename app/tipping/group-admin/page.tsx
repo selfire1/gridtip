8438876fd1eb4a3eccb0888df1ee40d87b23a04f
Joschua <70809675+selfire1@users.noreply.github.com>
2025-10-28 06:59:24 +1000

    start laying out group admin page

--- app/tipping/group-admin/page.tsx
+++ app/tipping/group-admin/page.tsx
@@ -1,7 +1,25 @@
-export default function GroupSettings() {
+import EmptyGroup from '@/components/empty-group'
+import { verifyIsAdmin, verifySession } from '@/lib/dal'
+import { getCurrentGroup } from '@/lib/utils/groups'
+import { redirect } from 'next/navigation'
+
+export default async function GroupSettings() {
+  const { userId } = await verifySession()
+  const group = await getCurrentGroup(userId)
+
+  if (!group) {
+    return <EmptyGroup />
+  }
+
+  const { isAdmin } = await verifyIsAdmin(group.id)
+
+  if (!isAdmin) {
+    redirect('/tipping')
+  }
+
   return (
     <>
-      <pre>GroupSettings</pre>
+      <h1 className='page-title'>{group.name}</h1>
     </>
   )
 }
