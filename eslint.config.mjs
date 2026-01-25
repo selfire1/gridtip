bed2ef6e5a2e0fb1cd40efff6bd2f27e0728b86a
Joschua <70809675+selfire1@users.noreply.github.com>
2026-01-25 12:20:09 +1000

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,32 +1,30 @@
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import nextTypescript from "eslint-config-next/typescript";
-import next from "eslint-config-next";
-import { FlatCompat } from '@eslint/eslintrc'
-import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
 
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-})
-
-const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, ...next, ...compat.config({
-  extends: ['prettier'],
-  plugins: ['prettier'],
-
-  rules: {
-    'react/no-children-prop': 'off',
-  }
-}), ...tseslint.configs.recommended, {
-  rules: {
-    '@typescript-eslint/no-unused-vars': [
-      'warn',
-      {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-      },
-    ],
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      'react/no-children-prop': 'off',
+    },
   },
-}, {
-  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
-}]
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+])
 
 export default eslintConfig
