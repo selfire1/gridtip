350f38609fb51fa657776c4c289032984de8af0b
Joschua <70809675+selfire1@users.noreply.github.com>
2026-01-26 12:21:57 +1000

    add link to privacy policy

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { Button } from '@/components/ui/button'
 import { AppHeader } from '../../components/app-header'
 
 export default function DefaultLayout({
@@ -21,8 +22,13 @@ export default function DefaultLayout({
 
 function AppFooter() {
   return (
-    <div className='border-t py-2'>
-      <p className='text-sm is-container text-muted-foreground'>
+    <div className='border-t py-2 flex items-center justify-between text-sm is-container text-muted-foreground'>
+      <Button asChild variant='link' className='text-muted-foreground'>
+        <Link href='/privacy' title='Privacy Policy'>
+          Privacy
+        </Link>
+      </Button>
+      <p>
         Built by{' '}
         <Link
           className='hover:text-foreground transition-colors'
