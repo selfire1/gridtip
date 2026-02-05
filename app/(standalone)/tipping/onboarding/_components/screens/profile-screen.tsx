f6895fdd9e9a5af81c82090477256cbb56d21655
Joschua <70809675+selfire1@users.noreply.github.com>
2026-02-05 17:37:09 +1000

    wip

--- app/(standalone)/tipping/onboarding/_components/screens/profile-screen.tsx
+++ app/(standalone)/tipping/onboarding/_components/screens/profile-screen.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import React from 'react'
 import ScreenLayout from '../screen-layout'
 import {
@@ -21,6 +22,19 @@ import {
 import { Input } from '@/components/ui/input'
 import { Avatar, AvatarImage } from '@/components/ui/avatar'
 import { getMyGroups } from '@/actions/get-my-groups'
+import { IconFromName } from '@/components/icon-from-name'
+import { Skeleton } from '@/components/ui/skeleton'
+import { LucideEye, LucideImageOff, LucideX } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+
+type ProfileState = {
+  name: string
+  image: string | undefined
+}
+
+type GroupProfileState = {
+  [groupId: string]: ProfileState
+}
 
 export default function ProfileScreen() {
   return (
@@ -39,9 +53,8 @@ export default function ProfileScreen() {
 }
 
 function ProfileContent() {
-  const [isPending, startTransition] = React.useTransition()
-
   const { data: session, isPending: isSessionPending } = authClient.useSession()
+  const [isFetching, startFetchTransition] = React.useTransition()
 
   const [groups, setGroups] =
     React.useState<Awaited<ReturnType<typeof getMyGroups>>>()
@@ -49,80 +62,139 @@ function ProfileContent() {
     if (!session?.user) {
       return
     }
-    getMyGroups().then(setGroups)
+    startFetchTransition(async () => {
+      const groups = await getMyGroups()
+      setGroups(groups)
+    })
   }, [session])
 
-  const [profileState, setProfileState] = React.useState<{
-    name: string
-    image: string | undefined
-  }>({
+  const [defaultProfile, setDefaultProfile] = React.useState<ProfileState>({
     name: '',
     image: undefined,
   })
+  const [groupProfiles, setGroupProfiles] = React.useState<GroupProfileState>(
+    {},
+  )
 
   React.useEffect(() => {
     if (isSessionPending || !session) {
       return
     }
-    setProfileState({
+    setDefaultProfile({
       name: session.user.name,
       image: session.user.profileImageUrl || session.user.image || undefined,
     })
   }, [session, isSessionPending])
 
+  const setDefaultName = (name: string) => {
+    setDefaultProfile((prev) => ({ ...prev, name }))
+  }
+
+  const setDefaultImage = (image: string | undefined) => {
+    setDefaultProfile((prev) => ({ ...prev, image }))
+  }
+
+  const setGroupName = (groupId: string, name: string) => {
+    setGroupProfiles((prev) => ({
+      ...prev,
+      [groupId]: { ...prev[groupId], name },
+    }))
+  }
+
+  const setGroupImage = (groupId: string, image: string | undefined) => {
+    setGroupProfiles((prev) => ({
+      ...prev,
+      [groupId]: { ...prev[groupId], image },
+    }))
+  }
+
   return (
-    <div className='w-full max-w-xl'>
-      <Card>
-        <CardHeader>
-          <CardTitle>Default Profile</CardTitle>
-          <CardDescription>
-            A group uses this profile for you, unless you specify one for that
-            group.
-          </CardDescription>
-        </CardHeader>
-        <CardContent className='space-y-8'>
-          <Field>
-            <FieldLabel htmlFor='name'>Name</FieldLabel>
-            <Input
-              autoComplete='name'
-              name='name'
-              value={profileState.name}
-              onChange={(e) =>
-                setProfileState((prev) => ({
-                  image: prev.image,
-                  name: e.target.value,
-                }))
-              }
-            ></Input>
-          </Field>
-          <FieldSet>
-            <FieldLegend>Image</FieldLegend>
-            <FieldGroup className='flex sm:flex-row'>
-              <Field className='sm:w-auto'>
-                <span className='sr-only'>Image Preview</span>
-                <div>
-                  <Avatar className='relative size-12 border border-muted-foreground'>
-                    {' '}
-                    <AvatarImage src={profileState.image} alt='' />
-                  </Avatar>
-                </div>
-              </Field>
-              <Field className='w-full'>
-                <FieldLabel className='sr-only' htmlFor='image'>
-                  Replace Image
-                </FieldLabel>
-                <Input onChange={handleImageChange} name='image' type='file' />
-                <FieldDescription>Select a picture to upload.</FieldDescription>
-              </Field>
-            </FieldGroup>
-          </FieldSet>
-        </CardContent>
-      </Card>
-      <pre>{JSON.stringify(groups, null, 2)}</pre>
+    <div className='w-full max-w-xl space-y-6'>
+      <GroupCardWrapper />
+      <ProfileCard
+        title='Default Profile'
+        description="If a group doesn't have a custom profile set, it will display this profile."
+        name={defaultProfile.name}
+        image={defaultProfile.image}
+        onNameChange={setDefaultName}
+        onImageChange={setDefaultImage}
+      />
     </div>
   )
 
-  function handleImageChange(e: React.ChangeEvent<HTMLInputElement>) {
+  function GroupCardWrapper() {
+    if (!groups?.length) {
+      return <div className='h-48'></div>
+    }
+    return (
+      <Card>
+        <CardHeader>
+          <CardTitle>Groups</CardTitle>
+          <CardDescription>
+            Customise your profile for each group.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className='space-y-4'>
+          {isFetching ? (
+            <>
+              <Skeleton className='h-32' />
+              <Skeleton className='h-32' />
+            </>
+          ) : (
+            <GroupCards />
+          )}
+        </CardContent>
+      </Card>
+    )
+  }
+  function GroupCards() {
+    if (!groups?.length) {
+      return (
+        <p className='text-sm text-muted-foreground italic text-center'>
+          No groups
+        </p>
+      )
+    }
+    return groups.map(({ group }) => (
+      <ProfileCard
+        key={group.id}
+        id={group.id}
+        title={group.name}
+        description='This profile will be used only for this group.'
+        icon={<IconFromName iconName={group.iconName} size={20} />}
+        name={groupProfiles[group.id]?.name ?? defaultProfile.name}
+        image={groupProfiles[group.id]?.image ?? defaultProfile.image}
+        onNameChange={(name) => setGroupName(group.id, name)}
+        onImageChange={(image) => setGroupImage(group.id, image)}
+      />
+    ))
+  }
+}
+
+type ProfileCardProps = {
+  id?: string
+  title: string
+  description: string | React.ReactNode
+  icon?: React.ReactNode
+  name: string
+  image: string | undefined
+  onNameChange: (name: string) => void
+  onImageChange: (image: string | undefined) => void
+}
+
+function ProfileCard({
+  title,
+  description,
+  id,
+  icon,
+  name,
+  image,
+  onNameChange,
+  onImageChange,
+}: ProfileCardProps) {
+  const [_, startTransition] = React.useTransition()
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0]
     if (!file) {
       return
@@ -130,12 +202,93 @@ function ProfileContent() {
     const reader = new FileReader()
     reader.onload = () => {
       startTransition(() => {
-        setProfileState((prev) => ({
-          image: reader.result as string,
-          name: prev.name,
-        }))
+        onImageChange(reader.result as string)
       })
     }
     reader.readAsDataURL(file)
   }
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className='flex items-center gap-2'>
+          {icon}
+          {title}
+        </CardTitle>
+        <CardDescription>
+          {id === 'global' ? (
+            <Alert className='mt-2 bg-amber-50 dark:bg-amber-950'>
+              <LucideEye />
+              <AlertTitle>Heads up!</AlertTitle>
+              <AlertDescription>
+                <p>
+                  This is a <span className='font-medium'> public group</span>.
+                  Make sure not to include any private information.
+                </p>
+              </AlertDescription>
+            </Alert>
+          ) : (
+            description
+          )}
+        </CardDescription>
+      </CardHeader>
+      <CardContent className='space-y-8'>
+        <Field>
+          <FieldLabel htmlFor={`name-${title}`}>Name</FieldLabel>
+          <Input
+            autoComplete='name'
+            id={`name-${title}`}
+            name='name'
+            value={name}
+            onChange={(e) => onNameChange(e.target.value)}
+          />
+        </Field>
+        <FieldSet>
+          <FieldLegend>Image</FieldLegend>
+          <FieldGroup className='flex sm:flex-row'>
+            <Field className='sm:w-auto'>
+              <span className='sr-only'>Image Preview</span>
+              <div className='relative'>
+                <Avatar className='size-12 border border-muted bg-muted'>
+                  <AvatarImage src={image} alt='' />
+                </Avatar>
+
+                {!image ? (
+                  <div className='absolute inset-0 flex items-center justify-center'>
+                    <LucideImageOff />
+                  </div>
+                ) : (
+                  <Button
+                    variant='ghost'
+                    disabled={!image}
+                    type='button'
+                    className='absolute -top-2 -right-2 z-10 rounded-full bg-default shadow-sm bg-red-100 hover:bg-red-200 dark:bg-red-900 hover:dark:bg-red-800 transition-colors'
+                    size='icon-xs'
+                    aria-label='Remove image'
+                    title='Remove image'
+                    onClick={() => onImageChange(undefined)}
+                  >
+                    <LucideX />
+                  </Button>
+                )}
+              </div>
+            </Field>
+            <Field className='w-full'>
+              <FieldLabel className='sr-only' htmlFor={`image-${title}`}>
+                Replace Image
+              </FieldLabel>
+              <Input
+                onChange={handleImageChange}
+                id={`image-${title}`}
+                accept='image/*'
+                name='image'
+                type='file'
+              />
+              <FieldDescription>Select a picture to upload.</FieldDescription>
+            </Field>
+          </FieldGroup>
+        </FieldSet>
+      </CardContent>
+    </Card>
+  )
 }
