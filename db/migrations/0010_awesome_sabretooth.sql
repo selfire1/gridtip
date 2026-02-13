ce7bf7b24087f2a2c8f71106d8de050cf134f81f
Joschua <70809675+selfire1@users.noreply.github.com>
2026-02-14 08:44:43 +1000

    regenerate migration

--- db/migrations/0010_awesome_sabretooth.sql
+++
@@ -1,4 +0,0 @@
-DROP INDEX `predictions_user_id_idx`;--> statement-breakpoint
-ALTER TABLE `predictions` ADD `member_id` text NOT NULL REFERENCES group_members(id);--> statement-breakpoint
-CREATE INDEX `predictions_member_id_idx` ON `predictions` (`member_id`);--> statement-breakpoint
-ALTER TABLE `predictions` DROP COLUMN `user_id`;
