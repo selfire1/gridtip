1f924cdbf8db783f105ae58a8c636736a3a56ec0
Joschua <70809675+selfire1@users.noreply.github.com>
2026-02-28 16:01:13 +1000

    disable sentry on local

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -6,6 +6,7 @@ import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
   dsn: 'https://54e3173ef1d7071e4f710c456ff88c04@o4510883461267456.ingest.de.sentry.io/4510883462447184',
+  enabled: process.env.NODE_ENV !== 'development',
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
