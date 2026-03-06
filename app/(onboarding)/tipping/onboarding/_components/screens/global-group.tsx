cd648a61c7cbfd19d17405ba5284e447e51603df
Joschua <70809675+selfire1@users.noreply.github.com>
2026-03-06 15:20:02 +1000

    refactor: extract wreath component

--- app/(onboarding)/tipping/onboarding/_components/screens/global-group.tsx
+++ app/(onboarding)/tipping/onboarding/_components/screens/global-group.tsx
@@ -8,6 +8,7 @@ import { Button } from '@/components/ui/button'
 import { useOnboarding } from '../../_lib/onboarding-context'
 import posthog from 'posthog-js'
 import { AnalyticsEvent } from '@/lib/posthog/events'
+import { WreathSide } from '@/components/wreath'
 
 export default function GlobalGroupScreen() {
   const { goToScreen, updateState } = useOnboarding()
@@ -88,49 +89,3 @@ export default function GlobalGroupScreen() {
 //     </svg>
 //   )
 // }
-
-function WreathSide({
-  side,
-  className,
-}: {
-  side: 'right' | 'left'
-  className?: string
-}) {
-  if (side === 'right') {
-    return (
-      <svg
-        xmlns='http://www.w3.org/2000/svg'
-        width='32'
-        height='32'
-        className={className}
-        viewBox='0 0 24 24'
-      >
-        <path
-          fill='none'
-          stroke='currentColor'
-          strokeLinejoin='round'
-          strokeWidth='1.5'
-          d='M16.756 6.611c.213 1.642-1.104 3.166-1.104 3.166s-1.663-1.138-1.877-2.78C13.562 5.353 14.88 3.83 14.88 3.83s1.663 1.139 1.877 2.781Zm1.561 5.519c-.696 1.503-2.624 2.093-2.624 2.093s-.8-1.847-.104-3.35s2.625-2.094 2.625-2.094s.8 1.848.103 3.351Zm-.82 5.285c-1.328.993-3.306.599-3.306.599s.176-2.005 1.504-2.998s3.306-.599 3.306-.599s-.176 2.005-1.504 2.998Zm-3.609 4.298c-1.505.698-3.359-.095-3.359-.095s.587-1.925 2.092-2.624c1.505-.698 3.358.096 3.358.096s-.586 1.925-2.091 2.623ZM12.22 2.968c1.21 1.133 1.164 3.144 1.164 3.144s-2.01.176-3.22-.957S9 2.01 9 2.01s2.01-.175 3.22.958Z'
-        />
-      </svg>
-    )
-  }
-
-  return (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      className={className}
-      width='32'
-      height='32'
-      viewBox='0 0 24 24'
-    >
-      <path
-        fill='none'
-        stroke='currentColor'
-        strokeLinejoin='round'
-        strokeWidth='1.5'
-        d='M9.246 6.611c-.213 1.642 1.104 3.166 1.104 3.166s1.663-1.138 1.876-2.78c.213-1.643-1.104-3.167-1.104-3.167S9.46 4.97 9.246 6.611ZM7.683 12.13c.696 1.503 2.624 2.093 2.624 2.093s.8-1.847.104-3.35s-2.624-2.094-2.624-2.094s-.8 1.848-.104 3.351Zm.821 5.285c1.328.993 3.306.599 3.306.599s-.176-2.005-1.504-2.998S7 14.417 7 14.417s.176 2.005 1.504 2.998Zm3.609 4.298c1.505.698 3.359-.095 3.359-.095s-.587-1.925-2.092-2.624c-1.505-.698-3.358.096-3.358.096s.586 1.925 2.091 2.623Zm1.668-18.745c-1.21 1.133-1.164 3.144-1.164 3.144s2.01.176 3.22-.957s1.164-3.145 1.164-3.145s-2.01-.175-3.22.958Z'
-      />
-    </svg>
-  )
-}
