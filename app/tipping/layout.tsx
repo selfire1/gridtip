6954593677e38e872011fc73a58a1e46b063d664
Joschua <70809675+selfire1@users.noreply.github.com>
2026-03-09 07:36:22 +1000

    add banner

--- app/tipping/layout.tsx
+++ app/tipping/layout.tsx
@@ -9,6 +9,7 @@ import Breadcrumbs from '../../components/breadcrumbs'
 import Link from 'next/link'
 import { LucideHome } from 'lucide-react'
 import { Button } from '@/components/ui/button'
+import Banner from '@/components/banner'
 
 export default async function TippingLayout({
   children,
@@ -19,6 +20,7 @@ export default async function TippingLayout({
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
+        <Banner />
         <header className='flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12'>
           <div className='flex items-center gap-2 px-4'>
             <SidebarTrigger className='-ml-1' />
