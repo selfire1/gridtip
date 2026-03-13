63089842e1a2dd32d8ac8367270a39811e177dff
Joschua <70809675+selfire1@users.noreply.github.com>
2026-03-13 14:33:52 +1000

    refactor: move shared functionality into component

--- components/select-constructor.tsx
+++ components/select-constructor.tsx
@@ -2,25 +2,10 @@
 
 import * as React from 'react'
 
-import { useMediaQuery } from '@/hooks/use-media-query'
-import {
-  Command,
-  CommandDialog,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from '@/components/ui/command'
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@/components/ui/popover'
 import ConstructorOption, {
   type ConstructorProps,
 } from '@/components/constructor'
-import { TriggerButton } from './select-trigger-button'
+import { ResponsiveSelect } from './responsive-select'
 
 export function SelectConstructor({
   constructors,
@@ -33,79 +18,20 @@ export function SelectConstructor({
   onSelect: (driver: ConstructorProps | undefined) => void
   disabled?: boolean
 }) {
-  const [open, setOpen] = React.useState(false)
-  const isDesktop = useMediaQuery('(min-width: 768px)')
-  const [selected, setSelected] = React.useState<ConstructorProps | undefined>(
-    constructors.find((d) => d.id === value?.id),
-  )
-  React.useEffect(() => {
-    setSelected(constructors.find((d) => d.id === value?.id))
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [value])
-
-  const triggerButtonProps = {
-    selectedItem: selected,
-    renderSelected: renderSelected,
-    emptyLabel: 'Select constructor',
-    onClick: () => setOpen(true),
-    disabled,
-  }
-
-  if (isDesktop) {
-    return (
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <TriggerButton {...triggerButtonProps} />
-        </PopoverTrigger>
-        <PopoverContent className='w-[300px] p-0' align='start'>
-          <ConstructorsList setOpen={setOpen} />
-        </PopoverContent>
-      </Popover>
-    )
-  }
-
   return (
-    <>
-      <TriggerButton {...triggerButtonProps} />
-      <CommandDialog open={open} onOpenChange={setOpen}>
-        <ConstructorsList setOpen={setOpen} />
-      </CommandDialog>
-    </>
+    <ResponsiveSelect
+      items={constructors}
+      value={value}
+      onSelect={onSelect}
+      disabled={disabled}
+      selectLabel='Select constructor'
+      searchLabel='Search constructors…'
+      renderSelected={(constructor) => (
+        <ConstructorOption constructor={constructor} isSelected={false} />
+      )}
+      renderItem={(constructor, isSelected) => (
+        <ConstructorOption constructor={constructor} isSelected={isSelected} />
+      )}
+    />
   )
-
-  function renderSelected(selected: ConstructorProps) {
-    return <ConstructorOption constructor={selected} isSelected={false} />
-  }
-
-  function ConstructorsList({ setOpen }: { setOpen: (open: boolean) => void }) {
-    return (
-      <Command>
-        <CommandInput placeholder='Search constructors…' />
-        <CommandList>
-          <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup>
-            {constructors.map((constructor) => (
-              <CommandItem
-                key={constructor.id}
-                value={constructor.id}
-                onSelect={(value) => {
-                  onSelect(
-                    constructors.find(
-                      (constructor) => constructor.id === value,
-                    ) || undefined,
-                  )
-                  setOpen(false)
-                }}
-              >
-                <ConstructorOption
-                  constructor={constructor}
-                  isSelected={selected?.id === constructor.id}
-                />
-              </CommandItem>
-            ))}
-          </CommandGroup>
-        </CommandList>
-      </Command>
-    )
-  }
 }
--- components/select-driver.tsx
+++ components/select-driver.tsx
@@ -2,23 +2,8 @@
 
 import * as React from 'react'
 
-import { useMediaQuery } from '@/hooks/use-media-query'
-import {
-  Command,
-  CommandDialog,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from '@/components/ui/command'
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@/components/ui/popover'
 import DriverOption, { DriverOptionProps } from '@/components/driver-option'
-import { TriggerButton } from './select-trigger-button'
+import { ResponsiveSelect } from './responsive-select'
 
 export function SelectDriver({
   drivers,
@@ -31,114 +16,20 @@ export function SelectDriver({
   onSelect: (driver: DriverOptionProps | undefined) => void
   disabled?: boolean
 }) {
-  const [open, setOpen] = React.useState(false)
-  const isDesktop = useMediaQuery('(min-width: 768px)')
-
-  const [selected, setSelected] = React.useState<DriverOptionProps | undefined>(
-    drivers.find((d) => d.id === value?.id),
-  )
-  React.useEffect(() => {
-    setSelected(drivers.find((d) => d.id === value?.id))
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [value])
-
-    const triggerButtonProps = {
-    selectedItem: selected,
-    renderSelected: renderSelected,
-    emptyLabel: 'Select driver',
-    onClick: () => setOpen(true),
-    disabled,
-  }
-
-
-  if (isDesktop) {
-    return (
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-      <TriggerButton {...triggerButtonProps} />
-        </PopoverTrigger>
-        <PopoverContent className='w-[300px] p-0' align='start'>
-          <DriverList setOpen={setOpen} />
-        </PopoverContent>
-      </Popover>
-    )
-  }
-
   return (
-    <>
-      <TriggerButton {...triggerButtonProps} />
-      <CommandDialog open={open} onOpenChange={setOpen}>
-        <DriverList setOpen={setOpen} />
-      </CommandDialog>
-    </>
+    <ResponsiveSelect
+      items={drivers}
+      value={value}
+      onSelect={onSelect}
+      disabled={disabled}
+      selectLabel='Select driver'
+      searchLabel='Search drivers…'
+      renderSelected={(driver) => (
+        <DriverOption driver={driver} isSelected={false} />
+      )}
+      renderItem={(driver, isSelected) => (
+        <DriverOption driver={driver} isSelected={isSelected} />
+      )}
+    />
   )
-
-    function renderSelected(selected: DriverOptionProps) {
-    return <DriverOption driver={selected} isSelected={false} />
-  }
-
-
-  function DriverList({ setOpen }: { setOpen: (open: boolean) => void }) {
-    return (
-      <Command>
-        <CommandInput placeholder='Search drivers…' />
-        <CommandList>
-          <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup>
-            {drivers.map((driver) => (
-              <CommandItem
-                key={driver.id}
-                value={getName(driver)}
-                onSelect={(name) => {
-                  onSelect(
-                    drivers.find((driver) => getName(driver) === name) ||
-                      undefined,
-                  )
-                  setOpen(false)
-                }}
-              >
-                <DriverOption
-                  driver={driver}
-                  isSelected={selected?.id === driver.id}
-                />
-              </CommandItem>
-            ))}
-          </CommandGroup>
-        </CommandList>
-      </Command>
-    )
-
-    function getName(driver: DriverOptionProps) {
-      return [driver.givenName, driver.familyName].join(' ')
-    }
-  }
-
-//   function TriggerButton({
-//     disabled,
-//     ...props
-//   }: {
-//     disabled?: boolean
-//   } & ShadButtonProps) {
-//     return (
-//       <FormControl>
-//         <Button
-//           type='button'
-//           disabled={disabled}
-//           variant='outline'
-//           className='justify-between flex'
-//           {...props}
-//         >
-//           {selected ? (
-//             <DriverOption driver={selected} isSelected={false} />
-//           ) : (
-//             <EmptyState />
-//           )}
-//           <ChevronsUpDown className='opacity-50' />
-//         </Button>
-//       </FormControl>
-//     )
-//     function EmptyState() {
-//       return <span>Select driver</span>
-//     }
-//   }
-// }
+}
