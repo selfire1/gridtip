a525261fd9650bc851eef72ed4365b578cd56e18
Joschua <70809675+selfire1@users.noreply.github.com>
2026-03-13 12:04:42 +1000

    chore: add api route to clear cache

---
+++ app/api/clear-cache/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest } from 'next/server'
+import { validateToken } from '../utils'
+import { CacheTag } from '@/constants/cache'
+import { revalidateTag } from 'next/cache'
+
+export const GET = async (_request: NextRequest) => {
+  const validationResponse = await validateToken()
+  if (!validationResponse.ok) {
+    return validationResponse
+  }
+
+  for (const tag in CacheTag) {
+    revalidateTag(tag)
+  }
+  return {
+    ok: true,
+    message: `Cleared ${Object.keys(CacheTag).length} tags`,
+  }
+}
