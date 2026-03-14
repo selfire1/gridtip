180f9e0c49378714748db76735193d196f523ea6
Joschua <70809675+selfire1@users.noreply.github.com>
2026-03-14 17:05:22 +1000

    fix: increase contrast on race tips

--- app/tipping/_components/card-group-tips.tsx
+++ app/tipping/_components/card-group-tips.tsx
@@ -226,7 +226,7 @@ export async function CardEveryonesTips({
             )}
           </div>
         </div>
-        <div className='space-y-3 p-3'>
+        <div className='space-y-3 p-3 bg-background'>
           {members.map((member) => (
             <div
               className={cn(
